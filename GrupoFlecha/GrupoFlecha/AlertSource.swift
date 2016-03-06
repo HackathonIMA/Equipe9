d44c6561bffb2ba833e4930ecd96b45ac66efae5
Davi Rodrigues <davirdgs@MacBook-Pro-de-Davi.local>
2016-03-06 06:42:11 -0300

    pods instalados

--- GrupoFlecha/GrupoFlecha/AlertSource.swift
+++ GrupoFlecha/GrupoFlecha/AlertSource.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import MapKit
+import Alamofire
+import SwiftyJSON
 
 class AlertSource: NSObject {
     
-    static let confirmadosUrl = "https://187.110.34.171/rest/confirmados"
-    static let focosUrl = "https://187.110.34.171/rest/focos"
+    static let confirmadosUrl = "http://187.110.34.171/rest/confirmados"
+    static let focosUrl = "http://187.110.34.171/rest/focos"
 
     static let alertaSimples = "    Atenção em um raio de 300 metros dessa localização, um caso de dengue foi confirmado. Caso você apresente: \n\n- Febre alta com início súbito. \n- Dor de cabeça. \n- Dor atrás dos olhos, que piora com o movimento deles. \n- Perda do paladar e apetite. \n- Náuseas e vômitos. \n- Tonturas. \n\n   Procure uma unidade de Saúde o mais rápido possível. Redobre a atenção com a água parada, eliminando possíveis criadouros. Além disso use repelentes e roupas compridas contra o mosquito da Dengue."
     
@@ -25,51 +27,21 @@ class AlertSource: NSObject {
     }
     
     static func getData() {
-        let url = NSURL(string: AlertSource.confirmadosUrl)
         
-        let session = NSURLSession.sharedSession()
-        
-        let request = NSMutableURLRequest(URL: url!)
-        
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {() -> Void in
-            }<#T##(NSURLResponse?, NSData?, NSError?) -> Void#>)
-        
-        do {
-        let contents = try NSString(contentsOfURL: url!, usedEncoding: nil)
-        print(contents)
-        } catch {
-            
+        var json = JSON(data: NSData())
+      
+        Alamofire.request(.GET, confirmadosUrl, parameters: ["foo": "bar"])
+            .responseJSON { response in
+                
+                json = JSON(data: response.data!)
+                
+                for index in 0...(json.count - 1) {
+                    print(json[index]["Latitude"].stringValue)
+                }
+                
+                
         }
 
         
-        
-        
-        request.HTTPMethod = "GET"
-        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
-        
-        //let paramString = "data=Hello"
-        //request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
-        
-        let task = session.dataTaskWithRequest(request) {
-            (let data, let response, let error) in
-            
-            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
-                print("error")
-                return
-            }
-
-            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            print(dataString)
-            
-        }
-        
-        task.resume()
-        
-        /*
-        let dataTask = session.dataTaskWithURL(url!, completionHandler: {(data:NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-                print(data)
-            })
-        
-    }*/
     }
 }
